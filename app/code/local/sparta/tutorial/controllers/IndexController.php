6d039c990e77e123af4172a6863a569d8df2b261
Sykander <sgul@spartaglobal.com>
2018-06-21 16:30:09 +0100

    made a module called tutorial with a controller which displays an index and a test page

---
+++ app/code/local/sparta/tutorial/controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+class sparta_tutorial_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo "Hello World";
+    }
+    public function testAction()
+    {
+        echo "test action";
+    }
+}
+
+ ?>
