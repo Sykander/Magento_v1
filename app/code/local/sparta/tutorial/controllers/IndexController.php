b1e4f8684c927c064ab13a422cdcb695095669bc
Sykander <sgul@spartaglobal.com>
2018-06-22 09:08:46 +0100

    made the index controller test page reference to the index controller index page for it's layout

--- app/code/local/sparta/tutorial/controllers/IndexController.php
+++ app/code/local/sparta/tutorial/controllers/IndexController.php
@@ -4,12 +4,15 @@ class sparta_tutorial_IndexController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
+      echo "index action";
       $this->loadLayout();
       $this->renderLayout();
     }
     public function testAction()
     {
         echo "test action";
+        $this->loadLayout();
+        $this->renderLayout();
     }
 }
 
